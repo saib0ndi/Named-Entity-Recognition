c6ae7e2dd4127afda4acd4be9619ce53a7737f50
Bondi Sai <126087452+ceo-prasanna@users.noreply.github.com>
2023-03-01 17:28:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import spacy
 from streamlit_option_menu import option_menu
 from streamlit_lottie import st_lottie
 import requests
-
+from spacy.cli import download
+download("en_core_web_sm")
 
 nlp = spacy.load("en_core_web_sm")
 
