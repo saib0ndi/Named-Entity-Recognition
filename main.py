6b2a224aceef58505b3a7e738e1466c9b926eda2
Bondi Sai <126087452+ceo-prasanna@users.noreply.github.com>
2023-03-02 09:45:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,9 @@ else:
     download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
 
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
 def main():
     
     st.markdown("<h1 style='text-align: center; color: #08e08a;'>Entity Recognizer</h1>", unsafe_allow_html=True)
-    lot = load_lottieurl('https://assets8.lottiefiles.com/packages/lf20_4kji20Y93r.json')
-    st_lottie(
-            lot,
-            speed=1,
-            reverse=False,
-            loop=True,
-            quality="High",
-            #renderer="svg",
-            height=400,
-            width=-900,
-            key=None,
-        )
     text = st.text_area("Enter the text to recognize entities")
     if st.button("Recognize"):
         
